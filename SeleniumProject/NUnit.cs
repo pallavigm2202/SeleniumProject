22b0d3aebab46d89122ee0916701445a3613cfd0
pallavi <pallavigm22@gmail.com>
2019-12-20 11:37:13 +0000

    New Project 2

---
+++ SeleniumProject/NUnit.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject
+{
+    class NUnit
+    {
+        public IWebDriver driver;
+       
+      
+        [Test]
+        public void Url()
+        {
+            driver = new ChromeDriver();
+            driver.Url = ("https://www.ebay.co.uk/");
+        }
+        [TearDown]
+        public void Browser()
+        {
+            driver.Close();
+        }
+    }
+}
